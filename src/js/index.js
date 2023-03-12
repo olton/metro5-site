fa19bc013104cd8e7e1770c05c57d3ca87fa015e
olton <serhii@pimenov.com.ua>
2023-03-12 19:47:32 +0200

    added indec

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,27 @@ globalThis.Metro = new Metro5({
 })
 
 import * as ResetCss from "@olton/metro5/src/common/reset"
-import * as Cloak from "@olton/metro5/src/components/cloak"
+import * as ColorCss from "@olton/metro5/src/common/colors"
+import * as FontCss from "@olton/metro5/src/common/font"
+import * as BorderCss from "@olton/metro5/src/common/border"
+import * as AnimationCss from "@olton/metro5/src/common/animation"
+import * as SpacingCss from "@olton/metro5/src/common/spacing"
+import * as FlexCss from "@olton/metro5/src/common/flex"
+import * as AlignCss from "@olton/metro5/src/common/align"
+import * as DisplayCss from "@olton/metro5/src/common/display"
+import * as TypographyCss from "@olton/metro5/src/common/typography"
+import * as ThemeCss from "@olton/metro5/src/common/theme"
+import * as Grid from "@olton/metro5/src/components/grid"
+import * as AppBar from "@olton/metro5/src/components/appbar"
+import * as Container from "@olton/metro5/src/components/container"
+import * as Hamburger from "@olton/metro5/src/components/hamburger"
+import * as Button from "@olton/metro5/src/components/button"
+import * as Progress from "@olton/metro5/src/components/progress"
+import * as ThemeToggle from "@olton/metro5/src/components/theme-toggle"
+import * as Cloak from "@olton/metro5/src/components/cloak/cloak.css?inline"
+import * as Mif from "@olton/metro5/icons"
+
+import "../css/site.css"
 
 const copyYear = $("#_copy-year")
 copyYear.html(`2019-${datetime().year()}`)
